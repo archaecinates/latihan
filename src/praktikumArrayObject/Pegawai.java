7e11fe61d2a4738258158518b8d2067f5ecd4af4
archaecinates <121710325+archaecinates@users.noreply.github.com>
2026-04-26 21:26:37 +0700

    Implement Pegawai class with attributes and methods
    Added attributes and methods for employee data management.

--- src/praktikumArrayObject/Pegawai.java
+++ src/praktikumArrayObject/Pegawai.java
@@ -1,4 +1,30 @@
 package praktikumArrayObject;
 
-public class Pegawai {
+public class Pegawai { //class data Pegawai
+    private int nip;
+    private String nama;
+    private String alamat;
+    private int umur;
+    private double gajiPokok;
+
+    public Pegawai(int nip, String nama, String alamat, int umur, double gajiPokok){ //constructor untuk menginisialisasi data pegawai
+        this.nip=nip;
+        this.nama=nama;
+        this.alamat=alamat;
+        this.umur=umur;
+        this.gajiPokok=gajiPokok;
+    }
+
+    public double getGajiPokok() { //method getter untuk mengambil nilai gaji pokok dari luar class
+        return gajiPokok; //mengembalikan isi gaji pokok
+    }
+
+    public void tampilData(){ //method untuk menampilkan seluruh data pegawai
+        System.out.println("\nDATA PEGAWAI"); //mencetak data pegawai
+        System.out.println("NIP: "+nip); //mencetak nip
+        System.out.println("Nama: "+nama); //mencetak nama
+        System.out.println("Alamat: "+alamat); //mencetak alamat
+        System.out.println("Umur: "+umur); //mencetak umur
+        System.out.println("Gaji Pokok: "+gajiPokok); //mencetak gaji pokok
+    }
 }
